dd2f47697095938733f31bbcf019baec3f74c9fc
Lander Corral <lander.corral97@gmail.com>
2021-05-10 12:42:52 +0200

    Introducido Rest Repositories

--- src/main/java/com/teknei/rest/coches/repositorios/CocheRepository.java
+++ src/main/java/com/teknei/rest/coches/repositorios/CocheRepository.java
@@ -2,13 +2,17 @@ package com.teknei.rest.coches.repositorios;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.teknei.rest.coches.modelo.Coches;
-import com.teknei.rest.coches.modelo.Marcas;
 
+@RepositoryRestResource(collectionResourceRel = "marcas", path = "marcas")
 public interface CocheRepository extends CrudRepository<Coches, Long> {
 
-	List<Coches> findByMarca(Marcas marca);
+	@Query("Select c From Coches c where c.marca = (Select m From Marcas m Where m.id = :id)")
+	List<Coches> findByMarca(@Param("id") Long id);
 
 }
--- src/main/java/com/teknei/rest/coches/repositorios/MarcaRepository.java
+++ src/main/java/com/teknei/rest/coches/repositorios/MarcaRepository.java
@@ -1,9 +1,11 @@
 package com.teknei.rest.coches.repositorios;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.teknei.rest.coches.modelo.Marcas;
 
+@RepositoryRestResource(collectionResourceRel = "marcas", path = "marcas")
 public interface MarcaRepository extends CrudRepository<Marcas, Long> {
 
 }
