f5920eca182ad5462917163bf76251dd575d3c01
Lander Corral <lander.corral97@gmail.com>
2021-05-10 10:45:03 +0200

    Configuración Inicial

---
+++ src/main/java/com/teknei/rest/coches/CochesRestApplication.java
@@ -0,0 +1,13 @@
+package com.teknei.rest.coches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CochesRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CochesRestApplication.class, args);
+	}
+
+}
