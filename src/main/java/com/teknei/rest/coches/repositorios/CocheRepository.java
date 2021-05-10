29486cc937aa1e2bb25c59b03668a26acee083fd
Lander Corral <lander.corral97@gmail.com>
2021-05-10 16:28:21 +0200

    Método innecesario quitado, se puede conseguir de otra forma.

--- src/main/java/com/teknei/rest/coches/repositorios/CocheRepository.java
+++ src/main/java/com/teknei/rest/coches/repositorios/CocheRepository.java
@@ -1,18 +1,20 @@
 package com.teknei.rest.coches.repositorios;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.teknei.rest.coches.modelo.Coches;
 
-@RepositoryRestResource(collectionResourceRel = "marcas", path = "marcas")
+/**
+ * 
+ * Repositorio Rest con Spring Data JPA. (Todas las funcionalidades)<br>
+ * En este caso es para los coches.
+ * 
+ * @author Lander Corral
+ * @Version 1.0
+ *
+ */
+@RepositoryRestResource(collectionResourceRel = "coches", path = "coches")
 public interface CocheRepository extends CrudRepository<Coches, Long> {
-
-	@Query("Select c From Coches c where c.marca = (Select m From Marcas m Where m.id = :id)")
-	List<Coches> findByMarca(@Param("id") Long id);
 
 }
