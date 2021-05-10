cb9b83ca07736dd9c4ef0f4e12dfbcca62af00be
Lander Corral <lander.corral97@gmail.com>
2021-05-10 16:29:02 +0200

    Repositorio de Marcas terminado

--- src/main/java/com/teknei/rest/coches/repositorios/MarcaRepository.java
+++ src/main/java/com/teknei/rest/coches/repositorios/MarcaRepository.java
@@ -5,6 +5,15 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.teknei.rest.coches.modelo.Marcas;
 
+/**
+ * 
+ * Repositorio Rest con Spring Data JPA. (Todas las funcionalidades) En este
+ * caso es para las marcas.
+ * 
+ * @author Lander Corral
+ * @Version 1.0
+ *
+ */
 @RepositoryRestResource(collectionResourceRel = "marcas", path = "marcas")
 public interface MarcaRepository extends CrudRepository<Marcas, Long> {
 
