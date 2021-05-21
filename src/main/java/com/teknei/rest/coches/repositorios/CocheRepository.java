8ddd636b7ae1f1e7b8f1d622ac923a739bb36f2c
lander-corral97 <lander.corral97@gmail.com>
2021-05-21 12:40:01 +0200

    Repositorios cambiados (no son RestRepository) Coche con método especial

--- src/main/java/com/teknei/rest/coches/repositorios/CocheRepository.java
+++ src/main/java/com/teknei/rest/coches/repositorios/CocheRepository.java
@@ -1,22 +1,22 @@
 package com.teknei.rest.coches.repositorios;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.teknei.rest.coches.modelo.Coches;
 
 /**
  * 
- * Repositorio Rest con Spring Data JPA. (Todas las funcionalidades)<br>
+ * Repositorio con Spring Data JPA. (Todas las funcionalidades + encontrar coche por id Marca)<br>
  * En este caso es para los coches.
  * 
  * @author Lander Corral
  * @Version 1.0
  *
  */
-@RepositoryRestResource(collectionResourceRel = "coches", path = "coches")
-@CrossOrigin(origins = "*")
 public interface CocheRepository extends CrudRepository<Coches, Long> {
 
+	@Query("Select c From Coches c Where marca.id = :id")
+	Iterable<Coches> findByMarcaId(Long id);
+	
 }
--- src/main/java/com/teknei/rest/coches/repositorios/MarcaRepository.java
+++ src/main/java/com/teknei/rest/coches/repositorios/MarcaRepository.java
@@ -1,22 +1,18 @@
 package com.teknei.rest.coches.repositorios;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.teknei.rest.coches.modelo.Marcas;
 
 /**
  * 
- * Repositorio Rest con Spring Data JPA. (Todas las funcionalidades) En este
- * caso es para las marcas.
+ * Repositorio con Spring Data JPA. (Todas las funcionalidades) <br>
+ * En este caso es para las marcas.
  * 
  * @author Lander Corral
  * @Version 1.0
  *
  */
-@RepositoryRestResource(collectionResourceRel = "marcas", path = "marcas")
-@CrossOrigin(origins = "*")
 public interface MarcaRepository extends CrudRepository<Marcas, Long> {
 
 }
