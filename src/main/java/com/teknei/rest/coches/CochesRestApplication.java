226678652a5e7991f7cd7a5b4e1c51e132cf2c69
lander-corral97 <lander.corral97@gmail.com>
2021-05-25 13:05:38 +0200

    Crear PDF y subir a Filenet ahora se hace con REST y no automático.

--- src/main/java/com/teknei/rest/coches/CochesRestApplication.java
+++ src/main/java/com/teknei/rest/coches/CochesRestApplication.java
@@ -1,34 +1,12 @@
 package com.teknei.rest.coches;
 
-import java.util.List;
-import java.util.Set;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.teknei.rest.coches.modelo.Coches;
-import com.teknei.rest.coches.modelo.Marcas;
-import com.teknei.rest.coches.util.FileNetUtil;
-import com.teknei.rest.coches.util.PDFUtil;
 
 @SpringBootApplication
 public class CochesRestApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CochesRestApplication.class, args);
-
-		List<Coches> coches = PDFUtil.sacarListaCoches();
-
-		Set<Marcas> marcas = PDFUtil.getMarcas(coches);
-
-		try {
-			for (Marcas marca : marcas) {
-				PDFUtil.crearPDF(coches, marca.getNombre());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		FileNetUtil.getObjectStore(marcas);
-
 	}
 }
--- src/main/java/com/teknei/rest/coches/controller/CocheMarcaController.java
+++ src/main/java/com/teknei/rest/coches/controller/CocheMarcaController.java
@@ -1,5 +1,8 @@
 package com.teknei.rest.coches.controller;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,6 +17,8 @@ import com.teknei.rest.coches.modelo.Coches;
 import com.teknei.rest.coches.modelo.Marcas;
 import com.teknei.rest.coches.repositorios.CocheRepository;
 import com.teknei.rest.coches.repositorios.MarcaRepository;
+import com.teknei.rest.coches.util.FileNetUtil;
+import com.teknei.rest.coches.util.PDFUtil;
 
 /**
  * Controlador para sacar Coches y Marcas,
@@ -73,5 +78,24 @@ public class CocheMarcaController {
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public Coches post(@RequestBody Coches coche) {
 		return cocheRepo.save(coche);
+	}
+	
+	/**
+	 * Crea los PDF y los sube al Filenet
+	 */
+	@GetMapping("/subir-filenet")
+	public void subirFilenet() {
+		List<Coches> coches = PDFUtil.sacarListaCoches();
+
+		Set<Marcas> marcas = PDFUtil.getMarcas(coches);
+
+		try {
+			for (Marcas marca : marcas) {
+				PDFUtil.crearPDF(coches, marca.getNombre());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		FileNetUtil.getObjectStore(marcas);
 	}
 }
