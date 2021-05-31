d94572d521e8dc44a041573ec6c847da3462c5cc
lander-corral97 <lander.corral97@gmail.com>
2021-05-31 10:42:22 +0200

    Añadido método de login. Subir Filenet saca la lista en el controlador.

--- src/main/java/com/teknei/rest/coches/controller/CocheMarcaController.java
+++ src/main/java/com/teknei/rest/coches/controller/CocheMarcaController.java
@@ -1,10 +1,10 @@
 package com.teknei.rest.coches.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +12,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.teknei.rest.coches.modelo.Coches;
 import com.teknei.rest.coches.modelo.Marcas;
-import com.teknei.rest.coches.modelo.Usuario;
 import com.teknei.rest.coches.repositorios.CocheRepository;
 import com.teknei.rest.coches.repositorios.MarcaRepository;
-import com.teknei.rest.coches.repositorios.UsuarioRepository;
+import com.teknei.rest.coches.servicio.JwtTokenService;
+import com.teknei.rest.coches.servicio.LoginService;
 import com.teknei.rest.coches.util.FileNetUtil;
 import com.teknei.rest.coches.util.PDFUtil;
 
@@ -41,10 +42,10 @@ public class CocheMarcaController {
 	private MarcaRepository marcaRepo;
 	
 	@Autowired
-	private UsuarioRepository usuarioRepo;
+	private LoginService loginService;
 	
 	@Autowired
-	private BCryptPasswordEncoder passEncoder;
+	private JwtTokenService jwtTokenService;
 	
 	/**
 	 * Saca la lista de Coches, con Marca
@@ -126,7 +127,8 @@ public class CocheMarcaController {
 	 */
 	@GetMapping("/subir-filenet")
 	public void subirFilenet() {
-		List<Coches> coches = PDFUtil.sacarListaCoches();
+		List<Coches> coches = new ArrayList<>();
+		cocheRepo.findAll().forEach(coches::add);
 
 		Set<Marcas> marcas = PDFUtil.getMarcas(coches);
 
@@ -140,12 +142,20 @@ public class CocheMarcaController {
 		FileNetUtil.getObjectStore(marcas);
 	}
 	
-	@PostMapping("/prueba")
-	public void pruebaInsert(@RequestBody Usuario usuario) {
-		String pwd = usuario.getPassword();
-		String encryptPwd = passEncoder.encode(pwd);
-		usuario.setPassword(encryptPwd);
-		usuarioRepo.save(usuario);
+	/**
+	 * Login del usuario en base a usuario y contraseña, devuelve un Token.
+	 * 
+	 * @param usuario Nombre del usuairo
+	 * @param password Contraseña del usuario
+	 * @return Token
+	 */
+	@GetMapping("/login")
+	public String login(@RequestParam("usuario") String usuario, @RequestParam("password") String password) {
+		if (loginService.login(usuario, password)) {
+			return jwtTokenService.getJWTToken(usuario);
+		} else {
+			return null;
+		}
 	}
 	
 }
