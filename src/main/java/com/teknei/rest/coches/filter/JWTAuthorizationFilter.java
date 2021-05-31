cacae87c59208f75b411e33f2603e31725b01b63
lander-corral97 <lander.corral97@gmail.com>
2021-05-31 10:40:52 +0200

    Filtro para acceder a los endpoint con autorización.

---
+++ src/main/java/com/teknei/rest/coches/filter/JWTAuthorizationFilter.java
@@ -0,0 +1,99 @@
+package com.teknei.rest.coches.filter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+/**
+ * Filtro para autorizar los endpoints
+ * 
+ * @author LANDER
+ *
+ */
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+
+	private final String HEADER = "Authorization";
+	private final String PREFIX = "Bearer ";
+	private final String SECRET = "clave_random";
+	
+	/**
+	 * Hace el filtro
+	 */
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		try {
+			if (existeJWTToken(request, response)) {
+				Claims claims = validateToken(request);
+				if (claims.get("authorities") != null) {
+					setUpSpringAuthentication(claims);
+				} else {
+					SecurityContextHolder.clearContext();
+				}
+			} else {
+					SecurityContextHolder.clearContext();
+			}
+			filterChain.doFilter(request, response);
+		} catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException e) {
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+			return;
+		}
+	}
+
+	/**
+	 * Valida el Token
+	 * 
+	 * @param request
+	 * @return
+	 */
+	private Claims validateToken(HttpServletRequest request) {
+		String jwtToken = request.getHeader(HEADER).replace(PREFIX, "");
+		return Jwts.parser().setSigningKey(SECRET.getBytes()).parseClaimsJws(jwtToken).getBody();
+	}
+
+	/**
+	 * Metodo para autenticarnos dentro del flujo de Spring
+	 * 
+	 * @param claims
+	 */
+	private void setUpSpringAuthentication(Claims claims) {
+		@SuppressWarnings("unchecked")
+		List<String> authorities = (List) claims.get("authorities");
+
+		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(claims.getSubject(), null,
+				authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+		SecurityContextHolder.getContext().setAuthentication(auth);
+
+		
+	}
+
+	/**
+	 * Mira si el token existe
+	 * @param request
+	 * @param res
+	 * @return Sí o No dependiendo de si existe
+	 */
+	private boolean existeJWTToken(HttpServletRequest request, HttpServletResponse res) {
+		String authenticationHeader = request.getHeader(HEADER);
+		if (authenticationHeader == null || !authenticationHeader.startsWith(PREFIX))
+			return false;
+		return true;
+	}
+}
