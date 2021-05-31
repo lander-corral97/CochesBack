09763cc82d2c55a9cc9d94e04e16efc84d761f01
lander-corral97 <lander.corral97@gmail.com>
2021-05-31 10:36:17 +0200

    Rol y Usuario documentados

--- src/main/java/com/teknei/rest/coches/modelo/Rol.java
+++ src/main/java/com/teknei/rest/coches/modelo/Rol.java
@@ -9,12 +9,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Roles de usuarios
+ * 
+ * @author LANDER
+ *
+ */
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Rol {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
--- src/main/java/com/teknei/rest/coches/modelo/Usuario.java
+++ src/main/java/com/teknei/rest/coches/modelo/Usuario.java
@@ -16,18 +16,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Usuarios, con su lista de roles.
+ * 
+ * @author LANDER
+ *
+ */
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Usuario {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
 	private String password;
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JoinTable(name="USUARIO_ROL",joinColumns=@JoinColumn(name="id_usuario"),inverseJoinColumns=@JoinColumn(name="id_rol"))
+	@JoinTable(name = "USUARIO_ROL", joinColumns = @JoinColumn(name = "id_usuario"), inverseJoinColumns = @JoinColumn(name = "id_rol"))
 	private Set<Rol> roles;
 }
