9530f38721dd245a36c68355df91065cebac1dd0
lander-corral97 <lander.corral97@gmail.com>
2021-06-03 13:48:00 +0200

    Cors Activado

--- src/main/java/com/teknei/rest/coches/config/SecurityConfig.java
+++ src/main/java/com/teknei/rest/coches/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.cors();
 		http.headers().frameOptions().disable();
 		http.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 		http.authorizeRequests().antMatchers("/login").permitAll().anyRequest().authenticated();
